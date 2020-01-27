e40a6a1a48e10957114b1d6ebdaeedba0916c300
Alessandro Duterte <alexduterte@gmail.com>
2020-01-27 22:26:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,5 +19,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  x = array[0]
+  array.length.times { |i|
+  if array[i] < x 
+     = array[i]
+  end
+  }
+  x
 end

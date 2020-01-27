4b5c4703ef8b4dcec27c949cab1831fe1280ec40
Alessandro Duterte <alexduterte@gmail.com>
2020-01-27 22:13:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
   i = 0
-  while i < array.length {
+  while i < array.length do
     if array[i] == value_to_find
       i
     end
-  }
+  end
 end
 
 def find_max_value(array)
